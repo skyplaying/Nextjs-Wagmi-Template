cb1fcf30f30557d61e2a9f981093ea0b3a5bb7a1
0xverin <daqingchong0809@gmail.com>
2025-06-06 00:52:51 +0800

    refactor: remove Ownable from TestToken contract and update deployment address

--- config/contracts/TestToken.sol
+++ config/contracts/TestToken.sol
@@ -3,17 +3,15 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
-    constructor(address initialOwner) 
+contract MyToken is ERC20, ERC20Burnable {
+    constructor() 
         ERC20("MyToken", "MTK") 
-        Ownable(initialOwner) 
     {
-        _mint(initialOwner, 10_000_000 * 10 ** decimals());
+        _mint(msg.sender, 10_000_000 * 10 ** decimals());
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
 
--- config/contracts/addresses.ts
+++ config/contracts/addresses.ts
@@ -5,7 +5,7 @@ export const ContractAddresses: Record<
   Record<number, `0x${string}` | "">
 > = {
   TestToken: {
-    [sepolia.id]: "0x59Cc3D82cfaEEe271E32Fbe812a103437DD91132",
+    [sepolia.id]: "0x2D0a6F1C3f427F5414a80Cfee844D7293ec8E668",
     // add more chains
     // [mainnet.id]: "0x59Cc3D82cfaEEe271E32Fbe812a103437DD91132",
   },
